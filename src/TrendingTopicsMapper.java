55ef0b77f3dc83b8d75bd467c4625d49901fce5b
albeertito7 <albertperezdatsira@gmail.com>
2021-05-13 17:56:30 +0200

    Update TrendingTopicsMapper.java

--- src/TrendingTopicsMapper.java
+++ src/TrendingTopicsMapper.java
@@ -2,6 +2,15 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class TrendingTopicsMapper extends Mapper<Object, Text, Text, IntWritable> {
+import java.io.IOException;
 
+public class TrendingTopicsMapper extends Mapper<Object, Text, Text, IntWritable>  {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+    {
+
+    }
 }
