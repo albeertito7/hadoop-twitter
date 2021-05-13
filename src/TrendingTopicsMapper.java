f1b8fe182ca7f4637dbb2de7f7a50b59a3048f03
albeertito7 <albertperezdatsira@gmail.com>
2021-05-13 17:56:12 +0200

    Create TrendingTopicsMapper.java

---
+++ src/TrendingTopicsMapper.java
@@ -0,0 +1,7 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class TrendingTopicsMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+}
