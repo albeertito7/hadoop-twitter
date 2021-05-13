3e224d390a8ae5f70eee793d2cff21c125d40e51
albeertito7 <albertperezdatsira@gmail.com>
2021-05-13 17:55:47 +0200

    Update TrendingTopics.java

--- src/TrendingTopics.java
+++ src/TrendingTopics.java
@@ -1,5 +1,8 @@
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import org.apache.hadoop.mapreduce.Job;
 
 public class TrendingTopics extends Configured implements Tool {
 
@@ -9,6 +12,7 @@ public class TrendingTopics extends Configured implements Tool {
     }
 
     public static void main(String[] args) throws Exception {
-
+        int exitCode = ToolRunner.run(new TrendingTopics(), args);
+        System.exit(exitCode);
     }
 }
