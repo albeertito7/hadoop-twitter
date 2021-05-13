f2ec65db9049279fc8a7c279a0e00e7a88c02360
albeertito7 <albertperezdatsira@gmail.com>
2021-05-13 17:55:37 +0200

    Create TrendingTopics.java

---
+++ src/TrendingTopics.java
@@ -0,0 +1,14 @@
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class TrendingTopics extends Configured implements Tool {
+
+    @Override
+    public int run(String[] strings) throws Exception {
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
