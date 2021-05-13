ab25a67878bce60bd5eaec0e3d5df19399094872
albeertito7 <albertperezdatsira@gmail.com>
2021-05-13 17:57:16 +0200

    Update TrendingTopicsReducer.java

--- src/TrendingTopicsReducer.java
+++ src/TrendingTopicsReducer.java
@@ -2,6 +2,25 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class TrendingTopicsReducer extends Reducer<Text, IntWritable, IntWritable,Text> {
 
+    @Override
+    public void setup(Context context) throws IOException, InterruptedException
+    {
+
+    }
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+    {
+
+    }
+
+    @Override
+    public void cleanup(Context context) throws IOException, InterruptedException
+    {
+
+    }
 }
