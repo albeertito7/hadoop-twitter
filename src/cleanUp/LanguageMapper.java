77d84d639845780b6a51c4506e066620edd471bd
albeertito7 <albertperezdatsira@gmail.com>
2021-05-25 20:39:30 +0200

    Update LanguageMapper.java

--- src/cleanUp/LanguageMapper.java
+++ src/cleanUp/LanguageMapper.java
@@ -1,7 +1,9 @@
 package cleanUp;
 
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
@@ -19,9 +21,11 @@ public class LanguageMapper extends Mapper<LongWritable, Text, LongWritable, Tex
         try {
             JSONObject json = new JSONObject(value.toString());
 
-            if (json.getString("lang").equals(language)) {
-                context.write(key, new Text(json.toString()));
+            if (!json.getString("lang").equals(language)) {
+                return;
             }
+
+            context.write(key, new Text(json.toString()));
         }
         catch (JSONException e) {
             Log.info("JSONException LanguageMapper", e.toString());
