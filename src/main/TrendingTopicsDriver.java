382d15a7fe325c2a91f19c164879a372e945f846
albeertito7 <albertperezdatsira@gmail.com>
2021-05-25 21:03:10 +0200

    Update TrendingTopicsDriver.java

--- src/main/TrendingTopicsDriver.java
+++ src/main/TrendingTopicsDriver.java
@@ -19,6 +19,8 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+import topN.TopNMapper;
+import topN.TopNReducer;
 import trendingTopics.TrendingTopicsMapper;
 import trendingTopics.TrendingTopicsReducer;
 
@@ -35,33 +37,43 @@ public class TrendingTopicsDriver extends Configured implements Tool {
 
         //JobControl jobControl = new JobControl("Trending Topics");
 
-        Job job = Job.getInstance(conf, "Trending Topics");
+        Job job1 = Job.getInstance(conf, "Trending Topics");
+        Job job2 = Job.getInstance(conf, "TopN");
 
-        job.setJarByClass(trendingTopics.TrendingTopicsDriver.class);
+        job1.setJarByClass(main.TrendingTopicsDriver.class);
+        job2.setJarByClass(topN.TopNDriver.class);
 
         // Setting the input and output path
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job1, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job1, new Path(args[1]));
 
         Path outputPath = new Path(args[1]);
         FileSystem fs = FileSystem.get(new URI(outputPath.toString()), conf);
 
         fs.delete(outputPath, true);
 
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
+        job1.setInputFormatClass(TextInputFormat.class);
+        job1.setOutputFormatClass(TextOutputFormat.class);
 
-        CleanUpDriver.cleanUp(job);
+        CleanUpDriver.cleanUp(job1);
 
-        ChainMapper.addMapper(job, TrendingTopicsMapper.class,
+        ChainMapper.addMapper(job1, TrendingTopicsMapper.class,
                 Object.class, Text.class, Text.class, IntWritable.class,
                 new Configuration(false));
 
-        ChainReducer.setReducer(job, TrendingTopicsReducer.class,
+        ChainReducer.setReducer(job1, TrendingTopicsReducer.class,
                 Text.class, IntWritable.class, IntWritable.class, Text.class,
-                conf);
+                new Configuration(false));
 
-        return (job.waitForCompletion(true) ? 0 : 1);
+        ChainMapper.addMapper(job2, TopNMapper.class,
+                Object.class, Text.class, Text.class, IntWritable.class,
+                new Configuration(false));
+
+        ChainReducer.setReducer(job2, TopNReducer.class,
+                Text.class, IntWritable.class, IntWritable.class, Text.class,
+                new Configuration(false));
+
+        return (job2.waitForCompletion(true) ? 0 : 1);
     }
 
     public static void main(String[] args) throws Exception {
