75fe518beb2ddc18a364516161908c4c745924cc
albeertito7 <albertperezdatsira@gmail.com>
2021-05-25 20:39:04 +0200

    Update TrendingTopicsMapper.java

--- src/trendingTopics/TrendingTopicsMapper.java
+++ src/trendingTopics/TrendingTopicsMapper.java
@@ -10,26 +10,18 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TrendingTopicsMapper extends Mapper<LongWritable, Text, Text, IntWritable>  {
+public class TrendingTopicsMapper extends Mapper<Object, Text, Text, IntWritable>  {
 
     private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
 
-    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException
     {
-        ArrayList<String> words = new ArrayList<>();
         Pattern pattern = Pattern.compile("(?:\\s|\\A|^)[##]+([A-Za-z0-9-_]+)"); // regular expression to access the hashtag
         Matcher matcher = pattern.matcher(value.toString());
-        matcher.matches();
-        matcher.groupCount();
 
-        if (matcher.find()) {
-            words.add(matcher.group(1));
-        }
-
-        for (String str: words) {
-            word.set(str);
-            context.write(word, one);
+        while (matcher.find()) {
+            String word = matcher.group(1).trim();
+            context.write(new Text(word), one);
         }
     }
 }
