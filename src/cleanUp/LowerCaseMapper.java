d1f17ba541d95990f4c03c7fcd5118bc72282dfc
albeertito7 <albertperezdatsira@gmail.com>
2021-05-25 20:39:27 +0200

    Update LowerCaseMapper.java

--- src/cleanUp/LowerCaseMapper.java
+++ src/cleanUp/LowerCaseMapper.java
@@ -1,19 +1,29 @@
 package cleanUp;
 
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class LowerCaseMapper extends Mapper<LongWritable, Text, Text, Text> {
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String word = key.toString().toLowerCase();
         // change tweets letter case to either lower-case
 
-        context.write(new Text(word), new Text(value.toString().toLowerCase()));
+        try {
+            JSONObject json = new JSONObject(value.toString());
+            json.put("text", json.getString("text").toLowerCase());
+
+            context.write(new Text(key.toString()), new Text(value.toString()));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
