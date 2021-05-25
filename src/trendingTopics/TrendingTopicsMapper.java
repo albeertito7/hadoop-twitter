b07bee8c302bdefb27da632f6a3a31dc5421fced
albeertito7 <albertperezdatsira@gmail.com>
2021-05-25 16:57:12 +0200

    Update TrendingTopicsMapper.java

--- src/trendingTopics/TrendingTopicsMapper.java
+++ src/trendingTopics/TrendingTopicsMapper.java
@@ -1,6 +1,7 @@
 package trendingTopics;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -9,12 +10,12 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TrendingTopicsMapper extends Mapper<Text, Text, Text, IntWritable>  {
+public class TrendingTopicsMapper extends Mapper<LongWritable, Text, Text, IntWritable>  {
 
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
 
-    public void map(Text key, Text value, Context context) throws IOException, InterruptedException
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
     {
         ArrayList<String> words = new ArrayList<>();
         Pattern pattern = Pattern.compile("(?:\\s|\\A|^)[##]+([A-Za-z0-9-_]+)"); // regular expression to access the hashtag
