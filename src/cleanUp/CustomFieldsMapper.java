c323731919822f1a7c483aca0a0e2c09e9db6889
albeertito7 <albertperezdatsira@gmail.com>
2021-05-25 20:39:34 +0200

    Update CustomFieldsMapper.java

--- src/cleanUp/CustomFieldsMapper.java
+++ src/cleanUp/CustomFieldsMapper.java
@@ -1,5 +1,6 @@
 package cleanUp;
 
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
