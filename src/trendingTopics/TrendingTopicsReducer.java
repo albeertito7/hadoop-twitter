747d1b434360bccb5cf2dcb1f63391126260e4ae
albeertito7 <albertperezdatsira@gmail.com>
2021-05-26 14:05:47 +0200

    Update TrendingTopicsReducer.java

--- src/trendingTopics/TrendingTopicsReducer.java
+++ src/trendingTopics/TrendingTopicsReducer.java
@@ -1,26 +1,15 @@
 package trendingTopics;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class TrendingTopicsReducer extends Reducer<Text, IntWritable, IntWritable, Text> {
 
-    private TreeMap<Integer, String> tMapRed;
-
     @Override
-    public void setup(Context context) throws IOException, InterruptedException
-    {
-        tMapRed = new TreeMap<>();
-    }
-
-    @Override
-    public void reduce(Text hashTag, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
     {
         int count = 0;
         for (IntWritable val : values)
@@ -28,24 +17,6 @@ public class TrendingTopicsReducer extends Reducer<Text, IntWritable, IntWritabl
             count += val.get();
         }
 
-        tMapRed.put(count, hashTag.toString());
-    }
-
-    /**
-     * This method is called once after all key/value pairs have been presented to the
-     * map method.
-     * @param context
-     * @throws IOException
-     * @throws InterruptedException
-     */
-    @Override
-    public void cleanup(Context context) throws IOException, InterruptedException
-    {
-        for (Map.Entry<Integer, String> entry : tMapRed.entrySet())
-        {
-            int hashTagCount = entry.getKey();
-            String name = entry.getValue();
-            context.write(new IntWritable(hashTagCount), new Text(name));
-        }
+        context.write(new IntWritable(count), new Text(key.toString()));
     }
 }
