1d9a7223713d8698e5de202529b374eef3d71251
albeertito7 <albertperezdatsira@gmail.com>
2021-05-26 14:04:33 +0200

    Update TrendingTopicsMapper.java

--- src/trendingTopics/TrendingTopicsMapper.java
+++ src/trendingTopics/TrendingTopicsMapper.java
@@ -1,12 +1,10 @@
 package trendingTopics;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
