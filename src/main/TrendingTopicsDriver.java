3b97157e50da17139b6a6131660bafabf6a25c34
albeertito7 <albertperezdatsira@gmail.com>
2021-05-26 20:52:29 +0200

    Update TrendingTopicsDriver.java

--- src/main/TrendingTopicsDriver.java
+++ src/main/TrendingTopicsDriver.java
@@ -2,7 +2,11 @@ package main;
 
 import cleanUp.CleanUpDriver;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.compress.DefaultCodec;
 import org.apache.hadoop.mapred.jobcontrol.JobControl;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -19,6 +23,7 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+import topN.TopNDriver;
 import topN.TopNMapper;
 import topN.TopNReducer;
 import trendingTopics.TrendingTopicsMapper;
@@ -26,52 +31,58 @@ import trendingTopics.TrendingTopicsReducer;
 
 import java.net.URI;
 
+import static cleanUp.CleanUpDriver.cleanUp;
+
 public class TrendingTopicsDriver extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
 
         Configuration conf = getConf();
+        conf.setInt("N", Integer.parseInt(args[2]));
 
         args = new GenericOptionsParser(conf, args).getRemainingArgs();
 
-        //JobControl jobControl = new JobControl("Trending Topics");
+        JobControl jobControl = new JobControl("Trending Topics");
 
-        Job job1 = Job.getInstance(conf, "Trending Topics");
-        Job job2 = Job.getInstance(conf, "TopN");
-
-        job1.setJarByClass(main.TrendingTopicsDriver.class);
-        job2.setJarByClass(topN.TopNDriver.class);
-
-        // Setting the input and output path
-        FileInputFormat.addInputPath(job1, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job1, new Path(args[1]));
-
-        Path outputPath = new Path(args[1]);
+        Path inputPath = new Path(args[0]), outputPath = new Path(args[1]),
+                trendingTopicOutputPath = new Path(outputPath + "/trendingtopic"),
+                cleanUpOutputPath = new Path(outputPath + "/cleanup"),
+                topNOutputPath = new Path(outputPath + "/topn");
         FileSystem fs = FileSystem.get(new URI(outputPath.toString()), conf);
-
         fs.delete(outputPath, true);
 
-        job1.setInputFormatClass(TextInputFormat.class);
-        job1.setOutputFormatClass(TextOutputFormat.class);
+        Job job1 = Job.getInstance(conf, "Clean Up");
+        job1.setJarByClass(CleanUpDriver.class);
+        cleanUp(job1);
+        job1.setOutputKeyClass(Text.class);
+        job1.setOutputValueClass(Text.class);
+        SequenceFileOutputFormat.setCompressOutput(job1, true);
+        SequenceFileOutputFormat.setOutputCompressorClass(job1, DefaultCodec.class);
+        SequenceFileOutputFormat.setOutputCompressionType(job1, SequenceFile.CompressionType.BLOCK);
+        FileInputFormat.addInputPath(job1, inputPath);
+        FileOutputFormat.setOutputPath(job1, cleanUpOutputPath);
+        ControlledJob controlledJob1 = new ControlledJob(conf);
+        controlledJob1.setJob(job1);
 
-        CleanUpDriver.cleanUp(job1);
 
-        ChainMapper.addMapper(job1, TrendingTopicsMapper.class,
-                Object.class, Text.class, Text.class, IntWritable.class,
-                new Configuration(false));
+        Job job2 = Job.getInstance(conf, "Trending Topics");
+        job2.setJarByClass(TrendingTopicsDriver.class);
+        job2.setMapperClass(TrendingTopicsMapper.class);
+        job2.setReducerClass(TrendingTopicsReducer.class);
+        job2.setOutputKeyClass(IntWritable.class);
+        job2.setOutputValueClass(Text.class);
 
-        ChainReducer.setReducer(job1, TrendingTopicsReducer.class,
-                Text.class, IntWritable.class, IntWritable.class, Text.class,
-                new Configuration(false));
+        Job job3 = Job.getInstance(conf, "TopN");
+        job3.setJarByClass(TopNDriver.class);
+        job3.setMapperClass(TopNMapper.class);
+        job3.setReducerClass(TopNReducer.class);
+        job3.setOutputKeyClass(IntWritable.class);
+        job3.setOutputValueClass(Text.class);
 
-        ChainMapper.addMapper(job2, TopNMapper.class,
-                Object.class, Text.class, Text.class, IntWritable.class,
-                new Configuration(false));
-
-        ChainReducer.setReducer(job2, TopNReducer.class,
-                Text.class, IntWritable.class, IntWritable.class, Text.class,
-                new Configuration(false));
+        /*jobControl.addJob(controlledJob1);
+        jobControl.addJob(controlledJob2);
+        jobControl.addJob(controlledJob3);*/
 
         return (job2.waitForCompletion(true) ? 0 : 1);
     }
