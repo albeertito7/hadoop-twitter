cb6737a74c21e73c8416fc6ebba8aa54c6a33dc9
albeertito7 <albertperezdatsira@gmail.com>
2021-05-28 20:15:46 +0200

    Update TrendingTopicsDriver.java

--- src/main/TrendingTopicsDriver.java
+++ src/main/TrendingTopicsDriver.java
@@ -1,14 +1,12 @@
 package main;
 
 import cleanUp.CleanUpDriver;
-import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.compress.DefaultCodec;
 import org.apache.hadoop.mapred.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -16,12 +14,8 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
-import org.apache.hadoop.mapreduce.lib.chain.ChainReducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import topN.TopNDriver;
 import topN.TopNMapper;
@@ -33,13 +27,15 @@ import java.net.URI;
 
 import static cleanUp.CleanUpDriver.cleanUp;
 
-public class TrendingTopicsDriver extends Configured implements Tool {
+public class TrendingTopicsDriver  {
 
-    @Override
-    public int run(String[] args) throws Exception {
+    private static final String language = "es";
 
-        Configuration conf = getConf();
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
         conf.setInt("N", Integer.parseInt(args[2]));
+        conf.setStrings("lang", new String[]{language});
 
         args = new GenericOptionsParser(conf, args).getRemainingArgs();
 
@@ -55,40 +51,56 @@ public class TrendingTopicsDriver extends Configured implements Tool {
         Job job1 = Job.getInstance(conf, "Clean Up");
         job1.setJarByClass(CleanUpDriver.class);
         cleanUp(job1);
-        job1.setOutputKeyClass(Text.class);
+        job1.setOutputKeyClass(NullWritable.class);
         job1.setOutputValueClass(Text.class);
-        SequenceFileOutputFormat.setCompressOutput(job1, true);
+        /*SequenceFileOutputFormat.setCompressOutput(job1, true);
         SequenceFileOutputFormat.setOutputCompressorClass(job1, DefaultCodec.class);
-        SequenceFileOutputFormat.setOutputCompressionType(job1, SequenceFile.CompressionType.BLOCK);
+        SequenceFileOutputFormat.setOutputCompressionType(job1, SequenceFile.CompressionType.BLOCK);*/
         FileInputFormat.addInputPath(job1, inputPath);
         FileOutputFormat.setOutputPath(job1, cleanUpOutputPath);
         ControlledJob controlledJob1 = new ControlledJob(conf);
         controlledJob1.setJob(job1);
 
-
         Job job2 = Job.getInstance(conf, "Trending Topics");
         job2.setJarByClass(TrendingTopicsDriver.class);
         job2.setMapperClass(TrendingTopicsMapper.class);
         job2.setReducerClass(TrendingTopicsReducer.class);
-        job2.setOutputKeyClass(IntWritable.class);
-        job2.setOutputValueClass(Text.class);
+        job2.setOutputKeyClass(Text.class);
+        job2.setOutputValueClass(IntWritable.class);
+        /*SequenceFileOutputFormat.setCompressOutput(job2, true);
+        SequenceFileOutputFormat.setOutputCompressorClass(job2, DefaultCodec.class);
+        SequenceFileOutputFormat.setOutputCompressionType(job2, SequenceFile.CompressionType.BLOCK);*/
+        FileInputFormat.addInputPath(job2, cleanUpOutputPath);
+        FileOutputFormat.setOutputPath(job2, trendingTopicOutputPath);
+        ControlledJob controlledJob2 = new ControlledJob(conf);
+        controlledJob2.setJob(job2);
 
-        Job job3 = Job.getInstance(conf, "TopN");
+        /*Job job3 = Job.getInstance(conf, "TopN");
         job3.setJarByClass(TopNDriver.class);
         job3.setMapperClass(TopNMapper.class);
         job3.setReducerClass(TopNReducer.class);
         job3.setOutputKeyClass(IntWritable.class);
         job3.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job3, trendingTopicOutputPath);
+        FileOutputFormat.setOutputPath(job3, topNOutputPath);
+        ControlledJob controlledJob3 = new ControlledJob(conf);
+        controlledJob3.setJob(job3);*/
 
-        /*jobControl.addJob(controlledJob1);
+        jobControl.addJob(controlledJob1);
         jobControl.addJob(controlledJob2);
-        jobControl.addJob(controlledJob3);*/
+        //jobControl.addJob(controlledJob3);
+        controlledJob2.addDependingJob(controlledJob1);
+        //controlledJob3.addDependingJob(controlledJob2);
 
-        return (job2.waitForCompletion(true) ? 0 : 1);
-    }
+        Thread thread = new Thread(jobControl);
+        thread.start();
 
-    public static void main(String[] args) throws Exception {
-        int exitCode = ToolRunner.run(new trendingTopics.TrendingTopicsDriver(), args);
-        System.exit(exitCode);
+        while(true){
+            if(jobControl.allFinished()){
+                System.out.println(jobControl.getSuccessfulJobList());
+                jobControl.stop();
+                break;
+            }
+        }
     }
 }
