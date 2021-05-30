157937877fafbdd7b20ac44d1d1c1c1f03cb1aba
albeertito7 <albertperezdatsira@gmail.com>
2021-05-30 18:08:44 +0200

    Create TrendingTopicsJSONMapper.java

---
+++ src/trendingTopics/TrendingTopicsJSONMapper.java
@@ -0,0 +1,34 @@
+package trendingTopics;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import java.io.IOException;
+
+public class TrendingTopicsJSONMapper extends Mapper<LongWritable, Text, Text, IntWritable>  {
+
+    private final static IntWritable one = new IntWritable(1);
+
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        try {
+            JSONObject json = new JSONObject(value.toString());
+            JSONArray values = json.getJSONArray("hashtags");
+
+            for(int i = 0; i < values.length(); ++i) {
+                JSONObject hashtag = values.getJSONObject(i);
+                String text = hashtag.getString("text");
+                if (!text.isEmpty()) {
+                    context.write(new Text(text), one);
+                }
+            }
+        } catch (JSONException var9) {
+            var9.printStackTrace();
+        }
+    }
+}
