3e3183e23db2e558c9d498ed8adc5f1325ec41b0
albeertito7 <albertperezdatsira@gmail.com>
2021-05-31 19:40:50 +0200

    Create SentimentMapper.java

---
+++ src/sentimentAnalysis/SentimentMapper.java
@@ -0,0 +1,72 @@
+package sentimentAnalysis;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class SentimentMapper extends Mapper<NullWritable, Text, Text, SentimentWritable> {
+
+    private static Set<String> positiveWords = new HashSet<>();
+    private static Set<String> negativeWords = new HashSet<>();
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        URI[] files = context.getCacheFiles();
+
+        Scanner scanner;
+        try {
+            scanner = new Scanner(new FileReader(files[0].getPath()));
+            while (scanner.hasNextLine()) {
+                positiveWords.add(scanner.nextLine().toLowerCase());
+            }
+
+            scanner = new Scanner(new FileReader(files[1].getPath()));
+            while (scanner.hasNextLine()) {
+                negativeWords.add(scanner.nextLine().toLowerCase());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void map(NullWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        try {
+            JSONObject json = new JSONObject(value.toString()); // data from cleanUp
+            String[] tokens = json.getString("text").split(" ");
+            JSONArray hashtags = json.getJSONArray("hashtags");
+
+            int counter = 0;
+            for (int i = 0; i < tokens.length; i++) {
+                if(positiveWords.contains(tokens[i])) {
+                    counter++;
+                }
+                else if (negativeWords.contains(tokens[i])) {
+                    counter--;
+                }
+            }
+
+            for (int i = 0; i < hashtags.length(); i++) {
+                context.write(new Text(hashtags.getString(i)), new SentimentWritable(new IntWritable(counter), new IntWritable(tokens.length)));
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
