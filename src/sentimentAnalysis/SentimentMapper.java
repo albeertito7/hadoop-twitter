bc429a164d4288640b0c3b9ffbac51638bb5fd79
albeertito7 <albertperezdatsira@gmail.com>
2021-06-01 15:35:00 +0200

    Update SentimentMapper.java

--- src/sentimentAnalysis/SentimentMapper.java
+++ src/sentimentAnalysis/SentimentMapper.java
@@ -1,5 +1,6 @@
 package sentimentAnalysis;
 
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -14,6 +15,7 @@ import java.net.URI;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 public class SentimentMapper extends Mapper<NullWritable, Text, Text, SentimentWritable> {
 
@@ -45,13 +47,17 @@ public class SentimentMapper extends Mapper<NullWritable, Text, Text, SentimentW
     protected void map(NullWritable key, Text value, Context context) throws IOException, InterruptedException {
 
         try {
+            Pattern pattern = Pattern.compile("(?:\\s|\\A|^)[##]+([A-Za-z0-9-_]+)");
             JSONObject json = new JSONObject(value.toString()); // data from cleanUp
             String[] tokens = json.getString("text").split(" ");
             JSONArray hashtags = json.getJSONArray("hashtags");
 
             int counter = 0;
             for (int i = 0; i < tokens.length; i++) {
-                if(positiveWords.contains(tokens[i])) {
+                if (pattern.matcher(tokens[i]).find()) {
+                    continue;
+                }
+                else if(positiveWords.contains(tokens[i])) {
                     counter++;
                 }
                 else if (negativeWords.contains(tokens[i])) {
@@ -60,7 +66,8 @@ public class SentimentMapper extends Mapper<NullWritable, Text, Text, SentimentW
             }
 
             for (int i = 0; i < hashtags.length(); i++) {
-                context.write(new Text(hashtags.getString(i)), new SentimentWritable(new IntWritable(counter), new IntWritable(tokens.length)));
+                JSONObject hashtag = hashtags.getJSONObject(i);
+                context.write(new Text(hashtag.getString("text")), new SentimentWritable(new FloatWritable(counter), new IntWritable(tokens.length)));
             }
 
 
