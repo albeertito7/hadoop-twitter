0d96cb39b49ffa0e3328e4c0a5dfb75df81e49af
albeertito7 <albertperezdatsira@gmail.com>
2021-06-01 15:35:05 +0200

    Update SentimentWritable.java

--- src/sentimentAnalysis/SentimentWritable.java
+++ src/sentimentAnalysis/SentimentWritable.java
@@ -3,29 +3,35 @@ package sentimentAnalysis;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Writable;
 
 public class SentimentWritable implements Writable {
-    private final IntWritable ratio;
-    private final IntWritable tweetLength;
+    private FloatWritable ratio;
+    private IntWritable tweetLength;
 
     public SentimentWritable() {
-        this.ratio = new IntWritable(0);
+        this.ratio = new FloatWritable(0);
         this.tweetLength = new IntWritable(0);
     }
 
-    public SentimentWritable(IntWritable ratio, IntWritable len) {
+    public SentimentWritable(FloatWritable ratio, IntWritable length) {
         this.ratio = ratio;
-        this.tweetLength = len;
+        this.tweetLength = length;
     }
 
-    public IntWritable getRatio() {
+    public FloatWritable getRatio() {
         return this.ratio;
     }
 
     public IntWritable getLength() {
         return this.tweetLength;
+    }
+
+    public void setRatio(FloatWritable ratio){
+        this.ratio = ratio;
     }
 
     public void write(DataOutput dataOutput) throws IOException {
