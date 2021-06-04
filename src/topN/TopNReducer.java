a42b796f91e641f33b3a79c94ff6d1e4af7d8c89
albeertito7 <albertperezdatsira@gmail.com>
2021-06-04 14:20:00 +0200

    Update TopNReducer.java

--- src/topN/TopNReducer.java
+++ src/topN/TopNReducer.java
@@ -1,8 +1,6 @@
 package topN;
 
-import org.apache.commons.lang.ObjectUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
